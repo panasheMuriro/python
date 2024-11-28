f8b87bebee19d3403fece9fb6519a3407d30e97a
murirop <panashemuriro2021@gmail.com>
2024-11-28 11:27:38 -0600

    add sqlalchecmy to the swagger doc

--- concepts/9.open_api_documentation.py
+++ concepts/9.open_api_documentation.py
@@ -6,13 +6,31 @@ Create an API for managing a book collection:
 Endpoints to add, list, update, and delete books.
 Swagger documentation for the API.
 """
-
 from flask import Flask, request, jsonify
 from flask_restx import Api, Resource, fields
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///books.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
 api = Api(app, version="1.0", title="Book API",
-          description="A simple Book Collection API")
+          description="A simple Book Collection API with SQLAlchemy")
+
+# Database Model
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(120), nullable=False)
+    author = db.Column(db.String(80), nullable=False)
+    year = db.Column(db.Integer)
+
+    def __repr__(self):
+        return f"<Book {self.title}>"
+
+# Create database tables
+with app.app_context():
+    db.create_all()
 
 # Book model for Swagger documentation
 book_model = api.model('Book', {
@@ -22,15 +40,6 @@ book_model = api.model('Book', {
     'year': fields.Integer(description="The year the book was published")
 })
 
-# In-memory data store
-books = []
-current_id = 1
-
-
-# Helper function to find a book by ID
-def find_book(book_id):
-    return next((book for book in books if book['id'] == book_id), None)
-
 
 # Endpoints
 @api.route('/books')
@@ -39,18 +48,17 @@ class BookList(Resource):
     @api.marshal_list_with(book_model)
     def get(self):
         """List all books"""
-        return books
+        return Book.query.all()
 
     @api.doc("create_book")
     @api.expect(book_model)
     @api.marshal_with(book_model, code=201)
     def post(self):
         """Add a new book"""
-        global current_id
-        new_book = request.json
-        new_book['id'] = current_id
-        current_id += 1
-        books.append(new_book)
+        data = request.json
+        new_book = Book(title=data['title'], author=data['author'], year=data.get('year'))
+        db.session.add(new_book)
+        db.session.commit()
         return new_book, 201
 
 
@@ -62,7 +70,7 @@ class Book(Resource):
     @api.marshal_with(book_model)
     def get(self, id):
         """Get a book by ID"""
-        book = find_book(id)
+        book = Book.query.get(id)
         if book is None:
             api.abort(404, "Book not found")
         return book
@@ -71,11 +79,11 @@ class Book(Resource):
     @api.response(204, "Book deleted")
     def delete(self, id):
         """Delete a book by ID"""
-        global books
-        book = find_book(id)
+        book = Book.query.get(id)
         if book is None:
             api.abort(404, "Book not found")
-        books = [b for b in books if b['id'] != id]
+        db.session.delete(book)
+        db.session.commit()
         return '', 204
 
     @api.doc("update_book")
@@ -83,11 +91,14 @@ class Book(Resource):
     @api.marshal_with(book_model)
     def put(self, id):
         """Update a book by ID"""
-        book = find_book(id)
+        book = Book.query.get(id)
         if book is None:
             api.abort(404, "Book not found")
-        updated_data = request.json
-        book.update(updated_data)
+        data = request.json
+        book.title = data.get('title', book.title)
+        book.author = data.get('author', book.author)
+        book.year = data.get('year', book.year)
+        db.session.commit()
         return book
 
 
