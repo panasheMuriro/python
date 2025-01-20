b60a99ba3f141d55940220b7f526c1e9b2e20715
murirop <panashemuriro2021@gmail.com>
2025-01-19 19:44:15 -0600

    add pyautogui

---
+++ automation/auto_gui.py
@@ -0,0 +1,13 @@
+import pyautogui
+
+# Move mouse to an absolute position
+# pyautogui.moveTo(100, 200, duration=1)  # Move to (100, 200) over 1 second
+
+# Move mouse relative to its current position
+# pyautogui.move(50, -50, duration=1)  # Move right by 50 and up by 50
+
+
+# screenshot = pyautogui.screenshot()
+# screenshot.save("screenshot2.png") 
+
+pyautogui.alert("This is a message box!")
